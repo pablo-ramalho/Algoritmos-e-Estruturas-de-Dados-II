2d3d1717e83ddb3fc3efa39275df3562a8d70d61
pablo-ramalho <94148839+pablo-ramalho@users.noreply.github.com>
2023-03-13 14:09:59 -0300

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,45 @@
+package Dijkstra;
+
+public class Node{
+
+    private Integer identificador;
+    private Integer peso;
+    private Node next;
+
+    public Node(Integer identificador, Integer peso){
+        this.setIdentificador(identificador);
+        this.setPeso(peso);
+
+    }
+
+    public Integer getIdentificador(){
+        return this.identificador;
+
+    }
+
+    public void setIdentificador(Integer identificador){
+        this.identificador = identificador;
+    
+    }
+
+    public Integer getPeso(){
+        return this.peso;
+    
+    }
+
+    public void setPeso(Integer peso){
+        this.peso = peso;
+
+    }
+
+    public Node getNext(){
+        return this.next;
+
+    }
+
+    public void setNext(Node next){
+        this.next = next;
+
+    }
+
+}
